3f97648936966555e88bfee2a9b511893594c232
Quinnbx <91432435+Quinnbx@users.noreply.github.com>
2023-04-07 19:24:10 -0400

    Initial Commit

---
+++ Vocabulary/VocabularyApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocabularyApp.swift
+//  Vocabulary
+//
+//  Created by Baiqi Xing on 4/7/23.
+//
+
+import SwiftUI
+
+@main
+struct VocabularyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
